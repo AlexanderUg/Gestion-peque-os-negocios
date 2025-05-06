8527199c33f7283479a9b4343099f12b856221ab
Alexander <alexanderugarteb@employeeslcdlc.com>
2025-05-06 12:53:35 +0200

    datos_es

--- laravel-app/database/migrations/2025_05_06_102001_create_products_table.php
+++ laravel-app/database/migrations/2025_05_06_102001_create_products_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->decimal('price',10,2);
-            $table->integer('initial_stock');
+            $table->string('nombre');
+            $table->text('descripcion');
+            $table->decimal('precio',10,2);
+            $table->integer('stock_inicial');
             $table->timestamps();
         });
     }
