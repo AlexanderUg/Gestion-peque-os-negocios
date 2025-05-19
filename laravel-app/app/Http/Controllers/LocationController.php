e455f9505f902e7c5a636ea436885f812fbdd780
Alexander <alexanderugarteb@employeeslcdlc.com>
2025-05-19 09:54:21 +0200

    dia 19

--- laravel-app/app/Http/Controllers/LocationController.php
+++ laravel-app/app/Http/Controllers/LocationController.php
@@ -5,18 +5,27 @@ namespace App\Http\Controllers;
 use App\Models\Location;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class LocationController extends Controller
 {
     public function index()
     {
          //$locations = Location::with('children')->whereNull('parent_id')->get();
-          $locations = Location::whereNull('parent_id')
+          /* $locations = Location::whereNull('parent_id')
                 ->with(['children' => function($query) {
                     $query->with('children'); // Carga los nietos
                 }])
-                ->get();
+                ->get(); */
+
        //  return view('locations.index', compact('locations')); 
+
+       $locations = Location::whereNull('parent_id')
+    ->with('children.children.children.children.children.children') // Puedes repetir según tu profundidad
+    ->get();
+
 
         return inertia('Locations/LocationIndex',compact('locations')); 
 
@@ -31,6 +40,7 @@ class LocationController extends Controller
     ]); */
  
     }
+
 
     public function create()
     {
